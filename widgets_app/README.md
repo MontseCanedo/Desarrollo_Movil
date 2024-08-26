a08ea867c9e6416c6ec14de488c881d6eab02c5d
MontseCanedo <montseykg@gmail.com>
2024-08-26 15:26:48 -0600

    cambio de carpeta

--- widgets_app/README.md
+++
@@ -1,3 +0,0 @@
-# widgets_app
-
-A new Flutter project.
--- widgets_app/android/app/src/main/kotlin/com/example/widgets_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.widgets_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
